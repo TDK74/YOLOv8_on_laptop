70c070455350504754760ee07fc291ecb59e22be
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-28 18:53:30 +0200

    Delete simple_cuda.py erased from the branch.

--- simple_cuda.py
+++
@@ -1,6 +0,0 @@
-import ultralytics
-from ultralytics import YOLO
-
-
-model = YOLO('yolov8n.pt')  # load an official model
-results = model(source='DiskName (C, D, E...):\path-to-folder\file_name.jpg', device='cuda')  # use of GPU for processing
