9ca0013bf18427a4a6afb9ca8a249012ed516e9a
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-03-07 18:40:18 +0200

    Update yolov8nsm-seg_gpu.py
    Corrections in the functions.

--- yolov8nsm-seg_gpu.py
+++ yolov8nsm-seg_gpu.py
@@ -23,21 +23,19 @@ height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 fps = 0
 
-def fps_calculation(fps, start_time, end_time):
-    loop_time = end_time - start_time
+def fps_calculation(s_time, e_time):
+    loop_time = e_time - s_time
     fps_func = 1 / loop_time
 
     return fps_func
 
 
-def objects_processing(frame):
-    results = model(frame, device='cuda')  # use of GPU for processing
+def objects_processing(framework):
+    results = model(framework, device='cuda')  # use of GPU for processing
 
     for result in results:
-
         pil_image = Image.fromarray(results[0].plot()[ : , : , : : -1])
-        frame = np.array(pil_image)
-
+        framework = np.array(pil_image)
         boxes = result.boxes.xyxy.cpu().numpy()
         confidences = result.boxes.conf.cpu().numpy()
         class_ids = result.boxes.cls.cpu().numpy().astype(int)
@@ -49,7 +47,7 @@ def objects_processing(frame):
             class_id = class_ids[i]
             label = result.names[class_id]
 
-    return frame
+    return framework
 
 
 try:
@@ -64,7 +62,7 @@ try:
         frame = objects_processing(frame)
 
         end_time = time()
-        fps = fps_calculation( fps, start_time, end_time)
+        fps = fps_calculation(start_time, end_time)
 
         cv2.putText(frame, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         #cv2.imshow('YOLOv8n-seg Real-time', frame)
