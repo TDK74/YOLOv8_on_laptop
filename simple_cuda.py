ed1c538f7158d50538856d9521a04062ab47c082
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-03-16 15:32:12 +0200

    Update simple_cuda.py - minor corrections in comments.

--- simple_cuda.py
+++ simple_cuda.py
@@ -2,5 +2,6 @@ import ultralytics
 from ultralytics import YOLO
 
 
-model = YOLO('yolov8n.pt')  # load an official model
+model = YOLO('yolov8n.pt')  # load an official PyTorch model
+# Enter your path to the file in the example path below
 results = model(source='DiskName (C, D, E...):\path-to-folder\file_name.jpg', device='cuda')  # use of GPU for processing
