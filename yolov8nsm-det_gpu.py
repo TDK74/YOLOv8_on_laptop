b7376a7ce0ae0ce7196097b82a7ca0ccfd11619f
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-03-16 15:42:04 +0200

    Update yolov8nsm-det_gpu.py - Added docstrings into the functions.

--- yolov8nsm-det_gpu.py
+++ yolov8nsm-det_gpu.py
@@ -22,6 +22,9 @@ height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = 0
 
 def fps_calculation(s_time, e_time):
+    """
+    Calculate frames per second.
+    """
     loop_time = e_time - s_time
     fps_func = 1 / loop_time
 
@@ -29,6 +32,9 @@ def fps_calculation(s_time, e_time):
 
 
 def objects_processing(framework):
+    """
+    Process detected objects and draw bounding boxes on the frame.
+    """
     results = model(framework, device='cuda')  # use of GPU for processing
 
     for result in results:
